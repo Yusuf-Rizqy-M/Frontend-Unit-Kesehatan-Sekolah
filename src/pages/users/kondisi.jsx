5c328c005ba9de5597782007ab8370151d65dc2c
Yusuf Rizqy M <yusufhabib290@gmail.com>
2025-03-17 10:38:28 +0700

    Fix: [YR] fix layout

--- src/pages/users/kondisi.jsx
+++ src/pages/users/kondisi.jsx
@@ -3,7 +3,7 @@ import Layout from '../../components/layout';
 function Kondisi() {
   return (
     <Layout>
-      <main className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <main>
         <h1 className="text-4xl text-teal-300">Welcome to Kondisi</h1>
         <p className="mt-2 text-gray-600">
           This is a React app built with Vite and Tailwind CSS.

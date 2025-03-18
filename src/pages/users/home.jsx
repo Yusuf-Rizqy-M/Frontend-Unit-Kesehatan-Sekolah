38956983a1d05573ba77d82f34339a9eb9d13868
Rakha Ghiyas Arrahman <yibi9817@gmail.com>
2025-03-18 11:29:32 +0700

    Feat: [RG] create-fasilities-teamuks-faq-section

--- src/pages/users/home.jsx
+++ src/pages/users/home.jsx
@@ -1,18 +1,22 @@
 import Layout from '../../components/layout';
 import HeroSection from '../../components/hero_section';
 import FeatureSection from '../../components/feature_section';
+import HealthcareTeam from "../../components/HealthcareTeam";
+import Facilities from "../../components/Facilities";
+import FAQ from "../../components/FAQ";
 
 function Home() {
   return (
     <Layout>
-      <main className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <main className="flex flex-col items-center justify-center min-h-screen bg-white">
         <HeroSection />
         <FeatureSection />
+        <HealthcareTeam />
+        <Facilities />
+        <FAQ />
       </main>
     </Layout>
   );
 }
 
 export default Home;
-
-
